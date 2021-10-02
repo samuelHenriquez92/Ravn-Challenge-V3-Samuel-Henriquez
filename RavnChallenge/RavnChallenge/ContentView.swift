26af7e9f3ac82fd022e075013ec5eab6810c9d6d
Samuel Henriquez <shenriquez@applaudostudios.com>
2021-10-01 20:49:43 -0600

    :tada: Begins initial configuration project

---
+++ RavnChallenge/RavnChallenge/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RavnChallenge
+//
+//  Created by Samuel Henriquez on 1/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RavnChallenge/RavnChallenge/RavnChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RavnChallengeApp.swift
+//  RavnChallenge
+//
+//  Created by Samuel Henriquez on 1/10/21.
+//
+
+import SwiftUI
+
+@main
+struct RavnChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
